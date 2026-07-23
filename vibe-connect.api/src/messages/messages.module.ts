758abbcf87a086a44b3ee75701a5b777d49a452a
Mert Rengül <mert.rengul@MERTRENGUL.local>
2026-07-23 13:07:07 +0300

    Minor Bug Fix

--- vibe-connect.api/src/messages/messages.module.ts
+++ vibe-connect.api/src/messages/messages.module.ts
@@ -4,10 +4,11 @@ import { MessagesController } from './messages.controller';
 import { MessagesGateway } from './messages.gateway';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
   providers: [MessagesService, MessagesGateway],
   controllers: [MessagesController],
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, CloudinaryModule],
 })
 export class MessagesModule {}
